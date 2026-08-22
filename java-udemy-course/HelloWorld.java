aff674d08708f4a09c3da88de97732ea809f4a0a
Anita Daniel <anitadaniel1977@gmail.com>
2026-08-22 11:10:48 +0200

    Add notes: how Java works (Udemy intro lesson 2)

---
+++ java-udemy-course/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {                              // Defines a class named HelloWorld every Java program needs at least one class
+    public static void main(String[] args) {            // The entry point this is the first method Java runs when the program starts
+        System.out.println("Hello, Java 8!");            // Prints text to the console, followed by a new line
+    }                                                     // Closes the main method
+}                                                         // Closes the class
